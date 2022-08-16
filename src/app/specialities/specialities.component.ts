b905c5d34ddf248f730df338d64c1cdc733b6b88
IsaquiasMarques <patisaquias2000@gmail.com>
2022-08-16 16:59:01 +0100

    changes

--- src/app/specialities/specialities.component.ts
+++ src/app/specialities/specialities.component.ts
@@ -7,19 +7,36 @@ import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild }
 })
 export class SpecialitiesComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('slide') slideElement !: ElementRef;
+  @ViewChild('specSlide') slideElement !: ElementRef;
+  @ViewChild('first') firstBoxElement !: ElementRef;
+  @ViewChild('second') secondBoxElement !: ElementRef;
+  @ViewChild('third') thirdBoxElement !: ElementRef;
+  @ViewChild('fourth') fourthBoxElement !: ElementRef
 
   constructor() { }
 
   isPrevClickable: boolean = false;
   isNextClickable: boolean = true;
 
+  slideEndOffset: number = 164;
+  slideElements: any;
+
+  firstBoxLeftOffset: number = 0;
+  secondBoxLeftOffset: number = 0;
+  thirdBoxLeftOffset: number = 0;
+  fourthBoxLeftOffset: number = 0;
+
   ngOnInit(): void {
 
   }
 
   ngAfterViewInit(): void {
-    
+    this.slideElements = this.slideElement.nativeElement;
+
+    this.firstBoxLeftOffset = this.firstBoxElement.nativeElement.offsetLeft;
+    this.secondBoxLeftOffset = this.secondBoxElement.nativeElement.offsetLeft;
+    this.thirdBoxLeftOffset = this.thirdBoxElement.nativeElement.offsetLeft;
+    this.fourthBoxLeftOffset = this.fourthBoxElement.nativeElement.offsetLeft;
   }
 
   @HostListener('window:wheel', ['$event']) listening(){ 
@@ -27,12 +44,23 @@ export class SpecialitiesComponent implements OnInit, AfterViewInit {
   }
 
   prevClick(){
-    this.isNextClickable = true; // Teste
+    this.isNextClickable = true;
     this.isPrevClickable = false;
+
+    this.prevWasClicked();
   }
   nextClick(){
-    this.isPrevClickable = true; // Teste
+    this.isPrevClickable = true;
     this.isNextClickable = false;
+
+    this.nextWasClicked();
+  }
+
+  nextWasClicked(){
+    this.slideElements.scrollLeft = this.fourthBoxLeftOffset - 100;
+  }
+  prevWasClicked(){
+    this.slideElements.scrollLeft = this.firstBoxLeftOffset - 100;
   }
 
 }
