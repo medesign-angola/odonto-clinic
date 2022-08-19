902377e3067632f02f0ab6c9c3e48fbd5b0b033f
IsaquiasMarques <patisaquias2000@gmail.com>
2022-08-19 11:00:13 +0100

    docs generated with spec slide

--- src/app/specialities/specialities.component.ts
+++ src/app/specialities/specialities.component.ts
@@ -33,10 +33,11 @@ export class SpecialitiesComponent implements OnInit, AfterViewInit {
   isOnThirdImage: boolean = false;
   isOnFourthImage: boolean = false;
 
-  childSlide: string = 25 + '%';
+  childSlide: string = 0 + '%';
 
   ngOnInit(): void {
 
+    this.childSlide = 25 + '%';
   }
 
   ngAfterViewInit(): void {
@@ -46,68 +47,9 @@ export class SpecialitiesComponent implements OnInit, AfterViewInit {
     this.secondBoxLeftOffset = this.secondBoxElement.nativeElement.offsetLeft;
     this.thirdBoxLeftOffset = this.thirdBoxElement.nativeElement.offsetLeft;
     this.fourthBoxLeftOffset = this.fourthBoxElement.nativeElement.offsetLeft;
-
-    let intevalToVerify = setInterval(() => {
-      this.checkUserPosition();
-    }, 400)
     
   }
 
-  @HostListener('window:wheel', ['$event'])
-  @HostListener('touchmove', ['$event'])
-  listening(){
-
-    this.slideTotal = this.slideElement.nativeElement.scrollLeft + this.slideElement.nativeElement.offsetLeft;
-    
-    // console.log(this.slideTotal);
-    // console.log(this.secondBoxLeftOffset);
-
-    // Verificação para mobile
-
-    if(this.slideTotal < this.secondBoxLeftOffset){
-
-      this.isOnFirstImage = true;
-      this.isOnSecondImage = false;
-      this.isOnThirdImage = false;
-      this.isOnFourthImage = false;
-
-    }else if(this.slideTotal >= this.secondBoxLeftOffset && this.slideTotal < this.thirdBoxLeftOffset){
-      
-      this.isOnFirstImage = false;
-      this.isOnSecondImage = true;
-      this.isOnThirdImage = false;
-      this.isOnFourthImage = false;
-
-    }else if(this.slideTotal >= this.thirdBoxLeftOffset && this.slideTotal < this.fourthBoxLeftOffset){
-      
-      this.isOnFirstImage = false;
-      this.isOnSecondImage = false;
-      this.isOnThirdImage = true;
-      this.isOnFourthImage = false;
-      
-    }else if(this.slideTotal + 20 >= this.fourthBoxLeftOffset){
-      
-      this.isOnFirstImage = false;
-      this.isOnSecondImage = false;
-      this.isOnThirdImage = false;
-      this.isOnFourthImage = true;
-    }
-
-    // Verificação para Desktop
-
-    if(this.slideTotal <= this.firstBoxLeftOffset){
-      this.isNextClickable = true;
-      this.isPrevClickable = false;
-
-    }else if(this.slideTotal > this.firstBoxLeftOffset ){
-      this.isNextClickable = false;
-      this.isPrevClickable = true;
-
-    }
-
-    this.checkUserPosition();
-
-  }
 
   prevClick(){
     if(this.isPrevClickable){
@@ -136,18 +78,113 @@ export class SpecialitiesComponent implements OnInit, AfterViewInit {
     this.slideElement.nativeElement.scrollTo(0, 0);
   }
 
+
+  
+  checkWhoIsActive(){
+    let interval = setInterval(() => {
+      this.conditionsOnScroll();
+    }, 500)
+
+    setTimeout(() => {
+      clearInterval(interval);
+    }, 1000)
+
+    // this.checkUserPosition();
+  }
+
+  // Capturar evento do toque na tela (telefone)
+  @HostListener('touchmove', ['$event']) touch(){
+    this.checkWhoIsActive();
+  }
+
+  // Capturar evento do scroll em um computador
+  @HostListener('window:wheel', ['$event'])
+  listening(){
+    // Validação da posição actual da lateral esquerda da tela para mobile
+
+    this.conditionsOnScroll();
+
+    // Validação da posição actual da lateral esquerda da tela para Desktop
+
+    if(this.slideTotal <= this.firstBoxLeftOffset){
+      this.isNextClickable = true;
+      this.isPrevClickable = false;
+
+    }else if(this.slideTotal > this.firstBoxLeftOffset ){
+      this.isNextClickable = false;
+      this.isPrevClickable = true;
+
+    }
+
+    // this.checkUserPosition();
+
+  }
+
+  conditionsOnScroll(){
+
+    this.slideTotal = this.slideElement.nativeElement.scrollLeft + this.slideElement.nativeElement.offsetLeft;
+    
+    // console.log(this.slideTotal);
+    // console.log(this.secondBoxLeftOffset);
+
+    if(this.slideTotal < this.secondBoxLeftOffset){
+
+      this.isOnFirstImage = true;
+      this.isOnSecondImage = false;
+      this.isOnThirdImage = false;
+      this.isOnFourthImage = false;
+
+      // Para cada verificação da posição do ecrã, função para actualizar a percentagem do custom scroll
+      this.checkUserPosition();
+
+    }else if(this.slideTotal >= this.secondBoxLeftOffset && this.slideTotal < this.thirdBoxLeftOffset){
+      
+      this.isOnFirstImage = false;
+      this.isOnSecondImage = true;
+      this.isOnThirdImage = false;
+      this.isOnFourthImage = false;
+
+      // Para cada verificação da posição do ecrã, função para actualizar a percentagem do custom scroll
+      this.checkUserPosition();
+
+    }else if(this.slideTotal >= this.thirdBoxLeftOffset && this.slideTotal < this.fourthBoxLeftOffset){
+      
+      this.isOnFirstImage = false;
+      this.isOnSecondImage = false;
+      this.isOnThirdImage = true;
+      this.isOnFourthImage = false;
+
+      // Para cada verificação da posição do ecrã, função para actualizar a percentagem do custom scroll
+      this.checkUserPosition();
+      
+    }else if(this.slideTotal + 20 >= this.fourthBoxLeftOffset){
+      
+      this.isOnFirstImage = false;
+      this.isOnSecondImage = false;
+      this.isOnThirdImage = false;
+      this.isOnFourthImage = true;
+      
+      // Para cada verificação da posição do ecrã, função para actualizar a percentagem do custom scroll
+      this.checkUserPosition();
+    }
+  }
+
   checkUserPosition(){
     if(this.isOnFirstImage){
       this.childSlide = 25 + '%';
+      // console.log("primeira imagem está activa");
 
     }else if(this.isOnSecondImage){
       this.childSlide = 50 + '%';
+      // console.log("segunda imagem está activa");
 
     }else if(this.isOnThirdImage){
       this.childSlide = 75 + '%';
+      // console.log("terceira imagem está activa");
 
     }else if(this.isOnFourthImage){
       this.childSlide = 100 + '%';
+      // console.log("quarta imagem está activa");
 
     }
   }
