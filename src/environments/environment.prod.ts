b416b44037520ed81f137802dcb1d85d85c71a5c
IsaquiasMarques <patisaquias2000@gmail.com>
2022-08-26 16:18:43 +0100

    teste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  token: "IGQVJVcWVSY0FiLXZAqbFdPQXRDdnNsUFZAva3hMRkNLblc2SHYybkhMbUtuUXVpWWh6blZASSHRqVmtIRlFLanFUZAVpmZAnktYU5MVjJVVXdFd1B4NFRtdDJsVXdCSjZAFQTNJM0syRGs2dEllUGJWbFYwSwZDZD",
 };
