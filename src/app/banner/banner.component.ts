ff4601d6b1a79f0a6ed4893561fd0d8b8701a03f
IsaquiasMarques <patisaquias2000@gmail.com>
2022-09-07 16:37:02 +0100

    new text transition

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -7,56 +7,32 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css'],
   animations: [
-    // trigger('fade', [
-    //   state('appear', style({
-    //     opacity: 0,
-    //     display: 'block'
-    //   })),
-    //   state('disappear', style({
-    //     opacity: 0,
-    //     display: 'none'
-    //   })),
-    //   transition('active => not-active', [
-    //     animate('1s'),
-    //   ]),
-    //   transition('not-active => active', [
-    //     animate('1s'),
-    //   ])
-    // ]),
   ]
 })
 export class BannerComponent implements OnInit {
 
   firstTextIsActive: boolean = true;
 
-  secondTextIsActive: any = 'off';
-  secondTextSentence: string = '';
+  secondTextIsActive: any = false;
+  // secondTextSentence: string = '';
 
-  thirdTextIsActive: any = 'off';
-  thirdTextSentence: string = '';
+  thirdTextIsActive: any = false;
+  // thirdTextSentence: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
-    
-    this.secondTextIsActive = false;
-    this.thirdTextIsActive = false;
 
     setTimeout(() => {
-
-      this.secondTextSentence = 'O sorriso é o seu, o cuidado é nosso.'
-      this.thirdTextSentence = 'Estou testando um texto aleatório aqui.';
-
-
       this.refresh();
-    }, 1500);
+    }, 2000);
     
   }
 
   refresh(){
     setInterval(() => {
       this.verification();
-    }, 3000)
+    }, 5000)
   }
 
   verification(){
